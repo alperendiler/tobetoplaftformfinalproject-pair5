a913d1c0f7ffb752c75ff9191c9f9c0572c13237
alperendiler <alperendiler1@gmail.com>
2024-01-17 17:00:24 +0300

    latest

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -73,40 +73,17 @@ const Login = () => {
 <div className="card mb-3">
 
   <div className="card-body">
-
-  <div className="d-flex justify-content-center py-4  d-flex align-items-center w-auto" >
-    <img className='login-logo' src="https://tobeto.com/_next/static/media/tobeto-logo.29b55e1c.svg" alt="" />
+  
+  <div className="mx-auto text-center col-md-6 col-12" >
+    <div className='ik-banner-big h-100'>
+    <img className='login-logo-istanbul' src="https://tobeto.com/_next/static/media/ik-logo-dark.7938c0de.svg" alt="" />
 </div>
-    <form className="row g-3 needs-validation" noValidate onSubmit={handleSubmit}>
 
-      <div className="col-12">
-        <label htmlFor="yourUsername" className="form-label">E Posta</label>
-        <div className="input-group has-validation">
-          <input type="text" name="username" className="form-control" id="yourUsername" required value={username} onChange={(e) => setUsername(e.target.value)}/>
-          <div className="invalid-feedback">Please enter your username.</div>
-        </div>
+    <span className="greenLine2"></span>
+    <div className="d-flex w-100 flex-column justify-content-center align-items-center">
+      <button className="btn istanbul-kodluyor-submit-btn d-md-inline-block mt-5 btn-dark">Başvur</button>
       </div>
-
-      <div className="col-12">
-        <label htmlFor="yourPassword" className="form-label">Şifre </label>
-        <input type="password" name="password" className="form-control" id="yourPassword" required value={password} onChange={(e) => setPassword(e.target.value)}/>
-        <div className="invalid-feedback">Please enter your password!</div>
-      </div>
-
-      <div className="col-12">
-        <div className="form-check">
-          <input className="form-check-input" type="checkbox" name="remember" value="true" id="rememberMe" checked={remember} onChange={(e) => setRemember(e.target.checked)}/>
-          <label className="form-check-label" htmlFor="rememberMe">Beni Hatırla</label>
-        </div>
-      </div>
-      <div className="col-12">
-        <button className="btn btn-primary w-100" type="submit">Giriş Yap</button>
-      </div>
-      <div className="col-12">
-        <p className="small mb-0">Henüz üye değil misin? <a href="pages-register.html">Kayıt Ol</a></p>
-      </div>
-    </form>
-
+    </div>
   </div>
   
 </div>
