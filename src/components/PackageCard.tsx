c3ac9ce9c58a1fe22b49e26e574d507f04d26f45
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-01-18 14:53:51 +0300

    footer, packagecardsrow, packagecard added

---
+++ src/components/PackageCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./packageCard.css"
+
+type Props = {
+    title: string; 
+};
+
+const PackageCard = ({title}: Props) => {
+  return (
+            <div className='col-md-4 col-sm-12 cardContainer'> 
+                <div className="profileCard col-12">
+                    <div>
+                        <h3 className='fw-bold'>{title}</h3>
+                        <button className='btn fw-bold rounded-pill cardbutton text-light '>Başla</button>
+                    </div>
+                </div>
+            </div>
+  )
+}
+
+export default PackageCard
