dfcf43ffe9e27b2e0c9f4999659a6f6510f56640
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-01-20 20:58:40 +0300

    competence corrected

---
+++ src/components/MyProfile/CompetenceCard.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../../styles/MyProfileStyles/competenceCard.css";
+
+type Competence = {
+  title: string;
+};
+
+type CompetenceCardProps = {
+  competence: Competence;
+};
+
+function CompetenceCard({ competence }: CompetenceCardProps) {
+  const { title } = competence;
+
+  return (
+    <div>
+      <div className="p-2">
+        <div className="row rounded-pill shadow mb-3 p-2 text-muted bg-white competenceStyles">
+          <div className="col-3">
+            <div>{title}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CompetenceCard;
