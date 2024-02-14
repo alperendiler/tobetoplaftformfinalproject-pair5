9f55cd6a1b4658982ff8cee0d621196e2ae4cfdb
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-02-14 15:18:48 +0300

    indent arranged

--- src/pages/ProfileInformation/MediaAccountInformation.tsx
+++ src/pages/ProfileInformation/MediaAccountInformation.tsx
@@ -8,8 +8,7 @@ import socialMediaService from "../../services/socialMediaService";
 import studentSocialMediaService from "../../services/socialMediaStudentService";
 import { GetSocialMediaStudentResponse } from "../../models/responses/SocialMediaStudentResponse.ts/getSocialMediaStudentResponse";
 import socialMediaStudentService from "../../services/socialMediaStudentService";
-import "../../styles/MyProfileStyles/socialMedia.css"
-
+import "../../styles/MyProfileStyles/socialMedia.css";
 
 type Props = {};
 interface MediaAccountForm {
@@ -37,12 +36,12 @@ export default function MediaAccountInformation({}: Props) {
   }, []);
   const fetchStudentSocialMedias = async () => {
     const userId = localStorage.getItem("userId")!;
-     const response = await studentSocialMediaService.GetListByUserId(
-       0,
-       50,
-       userId
-     );
-     setSocialMediaStudents(response.data.items);
+    const response = await studentSocialMediaService.GetListByUserId(
+      0,
+      50,
+      userId
+    );
+    setSocialMediaStudents(response.data.items);
   };
 
   const handleSubmit = async (socialMediaName: string, url: string) => {
@@ -100,15 +99,14 @@ export default function MediaAccountInformation({}: Props) {
                     >
                       <option value="">Seçiniz</option>
                       {socialMedias && socialMedias.length > 0 ? (
-  socialMedias.map((socialMedia) => ( 
-                 <option key={socialMedia.id}  value={socialMedia.id}>
-                   
-                    {socialMedia.name}
-                  </option>
-                  ))
-                  ) : (
-                   <div></div>
-                  )}  
+                        socialMedias.map((socialMedia) => (
+                          <option key={socialMedia.id} value={socialMedia.id}>
+                            {socialMedia.name}
+                          </option>
+                        ))
+                      ) : (
+                        <div></div>
+                      )}
                     </Field>
                   </div>
                   <div className="col-md-8 col-12">
@@ -130,83 +128,116 @@ export default function MediaAccountInformation({}: Props) {
           </div>
           <div>
             {socialMediaStudents.map((item, index) => (
-              <> 
-              <div className="m-2 text-muted">{item.socialMediaName && item.socialMediaName.charAt(0).toUpperCase() + item.socialMediaName.slice(1)}</div>
-              <div className="row mt-2 row-feature " key={index}>
-                <div className="col-10">
-                  <span>{item.socialMediaName}</span>-<span>{item.url}</span>
+              <>
+                <div className="m-2 text-muted">
+                  {item.socialMediaName &&
+                    item.socialMediaName.charAt(0).toUpperCase() +
+                      item.socialMediaName.slice(1)}
                 </div>
-                <div className="col-1">
-                  <button className="button-shape button-feature-1 border border-1">
-                  <svg
-                    xmlns="https://tobeto.com/trash.svg"
-                    width="23"
-                    height="23"
-                    fill="white"
-                    onClick={() => handleDelete(item.id)}
-                    className="ms-1 mt-1"
-                  >
-                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-                  </svg>
-                  </button>
+                <div className="row mt-2 row-feature " key={index}>
+                  <div className="col-10">
+                    <span>{item.socialMediaName}</span>-<span>{item.url}</span>
+                  </div>
+                  <div className="col-1">
+                    <button className="button-shape button-feature-1 border border-1">
+                      <svg
+                        xmlns="https://tobeto.com/trash.svg"
+                        width="23"
+                        height="23"
+                        fill="white"
+                        onClick={() => handleDelete(item.id)}
+                        className="ms-1 mt-1"
+                      >
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+                      </svg>
+                    </button>
+                  </div>
+                  <div className="col-1">
+                    <button
+                      className="button-shape button-feature-2"
+                      type="button"
+                      data-bs-toggle="modal"
+                      data-bs-target="#exampleModal"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="23"
+                        height="23"
+                        fill="white"
+                        className="bi bi-pencil-square ms-1 mt-1"
+                      >
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                        <path
+                          fill-rule="evenodd"
+                          d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
+                        />
+                      </svg>
+                    </button>
+                    <div
+                      className="modal fade"
+                      id="exampleModal"
+                      aria-labelledby="exampleModalLabel"
+                      aria-hidden="true"
+                    >
+                      <div className="modal-dialog modal-dialog-centered modal-lg">
+                        <div className="modal-content">
+                          <div className="modal-header">
+                            <h4 className="modal-title" id="exampleModalLabel">
+                              <b>Medya Hesabı Güncelleme</b>
+                            </h4>
+                          </div>
+                          <div className="modal-body row">
+                            <div className="col-md-4 ">
+                              <select
+                                className="form-select select-feature"
+                                name="socialMediaName"
+                                aria-label="Default select example"
+                              >
+                                <option value="">Seçiniz</option>
+                                {socialMedias && socialMedias.length > 0 ? (
+                                  socialMedias.map((socialMedia) => (
+                                    <option
+                                      key={socialMedia.id}
+                                      value={socialMedia.id}
+                                    >
+                                      {socialMedia.name}
+                                    </option>
+                                  ))
+                                ) : (
+                                  <div></div>
+                                )}
+                              </select>
+                            </div>
+                            <div className="col-md-8 ">
+                              <input
+                                className="edit-feature"
+                                name="socialMediaUrl"
+                                type="text"
+                                placeholder="https://"
+                              />
+                            </div>
+                          </div>
+                          <div className="modal-footer">
+                            <button
+                              type="button"
+                              className="btn btn-primary modal-button"
+                            >
+                              Güncelle
+                            </button>
+                            <button
+                              type="button"
+                              className="btn btn-secondary modal-button"
+                              data-bs-dismiss="modal"
+                            >
+                              Kapat
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div className="col-1">
-                  <button className="button-shape button-feature-2" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="23"
-                    height="23"
-                    fill="white"
-                    className="bi bi-pencil-square ms-1 mt-1"
-                   
-                  >
-                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                    <path
-                      fill-rule="evenodd"
-                      d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
-                    />
-                  </svg>
-                  </button>
-                  <div className="modal fade" id="exampleModal"  aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div className="modal-dialog modal-dialog-centered modal-lg">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h4 className="modal-title" id="exampleModalLabel"><b>Medya Hesabı Güncelleme</b></h4>
-      </div>
-      <div className="modal-body row">
-        <div className="col-md-4 ">
-        <select className="form-select select-feature"  name="socialMediaName" aria-label="Default select example">
-        <option value="">Seçiniz</option>
-                      {socialMedias && socialMedias.length > 0 ? (
-  socialMedias.map((socialMedia) => ( 
-                 <option key={socialMedia.id}  value={socialMedia.id}>
-                   
-                    {socialMedia.name}
-                  </option>
-                  ))
-                  ) : (
-                   <div></div>
-                  )} 
-</select>  
-        </div>
-        <div className="col-md-8 ">
-        <input className="edit-feature"
-                      name="socialMediaUrl"
-                      type="text"
-                      placeholder="https://"
-                    />
-        </div>
-      </div>
-      <div className="modal-footer"> 
-        <button type="button" className="btn btn-primary modal-button">Güncelle</button>
-        <button type="button" className="btn btn-secondary modal-button" data-bs-dismiss="modal">Kapat</button>
-      </div>
-    </div>
-  </div>
-</div>
-                </div>
-              </div>
               </>
             ))}
           </div>
