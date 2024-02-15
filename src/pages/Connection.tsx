a615ba1401f0a6e993deba3c9241b48dcaf1f2df
Busra <b.ertas91@gmail.com>
2024-02-15 17:36:56 +0300

    Button active or passive as recaptcha verified

--- src/pages/Connection.tsx
+++ src/pages/Connection.tsx
@@ -4,9 +4,13 @@ import { useState } from 'react';
 import FormikInput from "../components/FormikInput/FormikInput";
 import * as Yup from "yup";
 import { Formik, Form, Field } from "formik";
+import ReCAPTCHA from "react-google-recaptcha";
 
 type Props = {}
 
+const key = "6LeBsHMpAAAAABkznEGjEKudHplJpTumSoHiXL-y";
+function onChange(value: any) {
+}
 
 interface ConnectionForm {
     name: string;
@@ -28,10 +32,20 @@ const Connection = ({ }: Props) => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+    const [isCaptcha, setIsCaptcha] = useState(false);
 
     const handleSubmit = (e: any) => {
         e.preventDefault();
-        console.log('Form submitted:', { name, email, message });
+        if (isCaptcha) { 
+            console.log('Form submitted');
+            // Burada formun gerçek gönderim işlemleri yapılacak
+        } else {
+            console.log('Recaptcha not verified');  
+        }
+    };
+    const handleRecaptchaChange = (value: any) => {
+        setIsCaptcha(true);   //  Recaptcha doğrulandıysa
+                            //set'i true yap ve butonu aktif et
     };
 
     const validationSchema = Yup.object({
@@ -42,15 +56,29 @@ const Connection = ({ }: Props) => {
         email: Yup.string()
             .required("Doldurulması zorunlu alan*")
 
-
     });
 
     return (
-        <div className="container-fluid connection-all" style={{ backgroundColor: '#181717' }}>
-            <div className="container col-8 pt-5 mt-8 pb-5 align-items-center justify-content-center " style={{ backgroundColor: '#181717' }}>
-                <div className="row " style={{ marginTop: '55px', paddingTop: '20px', paddingLeft: '3px', paddingBottom: '100px', display: 'flex', backgroundColor: '#FFFFFF', borderRadius: '10px' }}>
-                    <div className="col-12 col-lg-6 pt-4 ml-8 shadow-lg d-flex flex-column align-items-center justify-content-center">
-                        <div><button className="top-span">İletişime Geçin</button></div>
+        <div className="container-fluid connection-all"
+            style={{ backgroundColor: '#181717' }}>
+
+            <div className="container col-8 pt-5 mt-8 pb-5 justify-content-center "
+                style={{ backgroundColor: '#181717' }}>
+
+                <div className="row "
+                    style={{
+                        marginTop: '25px',
+                        paddingTop: '20px',
+                        paddingLeft: '30px',
+                        paddingRight: '30px',
+                        paddingBottom: '100px',
+                        backgroundColor: '#FFFFFF',
+                        borderRadius: '10px'
+                    }}>
+                    <div className="course-left col-12 col-lg-6 pt-15 ml-8 
+                    shadow-lg justify-content-center">
+
+                        <div><button className="top-span ">İletişime Geçin</button></div>
                         <br />
                         <h2>İletişim Bilgileri</h2>
                         <div className="table-cn">
@@ -103,7 +131,7 @@ const Connection = ({ }: Props) => {
                     </div>
 
 
-                    <div className="col-12 col-lg-6 pl-6 pt-5  shadow-lg d-flex flex-column align-items-center justify-content-center ">
+                    <div className="course-right col-12 col-lg-6 pl-6 pt-5  shadow-lg justify-content-center ">
                         <div><button className="top-span">Mesaj Bırakın</button></div>
                         <br />
                         <h2>İletişim Formu</h2>
@@ -114,6 +142,7 @@ const Connection = ({ }: Props) => {
                                 console.log(values);
                             }}
                         >
+
 
                             <form className="contact-form" onSubmit={handleSubmit}>
                                 <label htmlFor="name"></label>
@@ -156,11 +185,21 @@ const Connection = ({ }: Props) => {
                                 <div className="enoc">
                                     Yukarıdaki form ile toplanan kişisel verileriniz Enocta tarafından talebinize dair işlemlerin yerine getirilmesi ve paylaşmış olduğunuz iletişim adresi üzerinden tanıtım, bülten ve pazarlama içerikleri gönderilmesi amacıyla <a href="#">Aydınlatma Metni </a>çerçevesinde işlenebilecektir.
                                 </div><br />
+                                <div className="capture-style mt-2">
+                                    <ReCAPTCHA
+                                        sitekey={key}
+                                        size="normal"
+                                        hl="tr"
+                                        theme="light"
+                                        onChange={handleRecaptchaChange}
+                                    />
+                                </div><br />
                                 <div className="connection-button">
-                                    <button type="submit">Gönder</button></div>
+                                    <button type="submit" disabled={!isCaptcha}>Gönder</button></div>
+                                    {/* isCaptcha true(doğrulanma) olmuşsa disabled false olacak ve buton görünecek */}
                             </form>
-                        </Formik>
 
+                        </Formik>
                     </div>
 
                 </div>
